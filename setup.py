6c3b05bfa6d9c6f58c6e55952c3b03a743a788eb
caleblareau <caleblareau@g.harvard.edu>
2018-05-03 18:46:45 -0400

    fixed version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dependencies = ['click', 'pytest', 'snakemake', 'optparse-pretty', 'regex', 'pys
 
 setup(
     name='bap',
-    version='0.2.3',
+    version='0.2.4',
     url='https://github.com/caleblareau/bap',
     license='MIT',
     author='Caleb Lareau',
