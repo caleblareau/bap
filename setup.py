988a7e3c53cb1725259fcf51e6d8e204186f3911
caleblareau <caleblareau@g.harvard.edu>
2019-03-01 13:25:08 -0500

    added GRC convention reference genomes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dependencies = ['python-Levenshtein','biopython','fuzzysearch','fuzzywuzzy','cli
 
 setup(
     name='bap',
-    version='0.5.4a',
+    version='0.5.5',
     url='https://github.com/caleblareau/bap',
     license='MIT',
     author='Caleb Lareau',
