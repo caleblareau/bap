2e2598505ec1175d3d3b158f010daab4c72c8656
caleblareau <caleblareau@g.harvard.edu>
2019-07-09 21:43:50 -0500

    tweaks

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dependencies = ['python-Levenshtein','biopython','fuzzysearch','fuzzywuzzy','cli
 
 setup(
     name='bap',
-    version='0.5.9f',
+    version='0.5.9g',
     url='https://github.com/caleblareau/bap',
     license='MIT',
     author='Caleb Lareau',
